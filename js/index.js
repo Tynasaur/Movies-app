e85e48d60a21160e32a88e3f3b74503fcb058b54
Tynasaur <70995431+Tynasaur@users.noreply.github.com>
2021-03-24 19:02:36 -0500

    Updated index.js with express.static

--- js/index.js
+++ js/index.js
@@ -1,72 +1,105 @@
-const express = require('express'),
-morgan = require('morgan');
+// const express = require('express'),
+// morgan = require('morgan');
 
+// const app = express();
+
+// app.use(morgan('common'));
+
+// let topMovies = [
+//   {
+//     title: 'How\'s Moving Castle',
+//     director: 'Hayo Miyazaki'
+//   },
+//   {
+//     title: 'Fifth Element',
+//     director: 'Luc Besson'
+//   },
+//   {
+//     title: 'Galaxy Quest',
+//     director: 'Dean Parisot'
+//   },
+//   {
+//     title: 'Star Wars: Return of the Jedi',
+//     director: 'Richard Marquand'
+//   },
+//   {
+//     title: 'Inglorious Basterds',
+//     director: 'Quentin Tarantino'
+//   },  
+//   {
+//     title: 'Aliens',
+//     director: 'Ridley Scott'
+//   },
+//   {
+//     title: 'Heathers',
+//     director: 'Michael Lehman'
+//   },  
+//   {
+//     title: 'Interstellar',
+//     director: 'Chistopher Nolan'
+//   },
+//   {
+//     title: 'Kill Bill',
+//     director: 'Quentin Tarantino'
+//   },  {
+//     title: 'Gone With the Wind',
+//     director: 'Victor Fleming'
+//   }
+// ];
+
+// // GET requests
+// app.get('/', (req, res) => {
+//   res.send('Welcome to my movie club!');
+// });
+
+// app.use(express.static('public'));
+
+// app.get('/movies', (req, res) => {
+//   res.json(topMovies);
+// });
+
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send('What did you do now?');
+// });
+
+
+// // listen for requests
+// app.listen(8080, () => {
+//   console.log('Your app is listening on port 8080.');
+// });
+const express = require('express');
 const app = express();
 
-app.use(morgan('common'));
-
-let topMovies = [
+let topBooks = [
   {
-    title: 'How\'s Moving Castle',
-    director: 'Hayo Miyazaki'
+    title: 'Harry Potter and the Sorcerer\'s Stone',
+    author: 'J.K. Rowling'
   },
   {
-    title: 'Fifth Element',
-    director: 'Luc Besson'
+    title: 'Lord of the Rings',
+    author: 'J.R.R. Tolkien'
   },
   {
-    title: 'Galaxy Quest',
-    director: 'Dean Parisot'
-  },
-  {
-    title: 'Star Wars: Return of the Jedi',
-    director: 'Richard Marquand'
-  },
-  {
-    title: 'Inglorious Basterds',
-    director: 'Quentin Tarantino'
-  },  
-  {
-    title: 'Aliens',
-    director: 'Ridley Scott'
-  },
-  {
-    title: 'Heathers',
-    director: 'Michael Lehman'
-  },  
-  {
-    title: 'Interstellar',
-    director: 'Chistopher Nolan'
-  },
-  {
-    title: 'Kill Bill',
-    director: 'Quentin Tarantino'
-  },  {
-    title: 'Gone With the Wind',
-    director: 'Victor Fleming'
+    title: 'Twilight',
+    author: 'Stephanie Meyer'
   }
 ];
 
 // GET requests
 app.get('/', (req, res) => {
-  res.send('Welcome to my movie club!');
+  res.send('Welcome to my book club!');
 });
 
-app.get('/documentation', (req, res) => {                  
-  res.sendFile('public/documentation.html', { root: __dirname });
-});
 
-app.get('/movies', (req, res) => {
-  res.json(topMovies);
-});
+app.use(express.static('public'));
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('What did you do now?');
+app.get('/books', (req, res) => {
+  res.json(topBooks);
 });
 
 
 // listen for requests
-app.listen(8080, () => {
+app.listen(8080, () =>{
   console.log('Your app is listening on port 8080.');
 });
