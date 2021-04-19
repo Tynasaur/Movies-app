f74f9010d7fb9c40c0d601161b3d81a690b31a68
Tynasaur <70995431+Tynasaur@users.noreply.github.com>
2021-04-19 10:24:55 -0500

    fixed typo

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ app.post('/users',
       return res.status(422).json({ errors: errors.array() })
     }
 
-    let hashedPassword = Users.hashedPassword(req.body.Password);
+    let hashedPassword = Users.hashPassword(req.body.Password);
     Users.findOne({ Username: req.body.Username }) //Search to see if a user with the requested username is already being used
       .then((user) => {
         if (user) {
